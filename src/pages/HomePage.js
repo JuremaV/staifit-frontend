6eaa78b228198dc92eb430486df124f069d3f8f2
Jurema Vidal <juremavidal.jv62@gmail.com>
2024-08-18 14:01:00 +0200

    Initial commit for stAIfit frontend

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to stAIfit</h1>
+      <p>Your AI-driven fitness companion.</p>
+    </div>
+  );
+};
+
+export default HomePage;
