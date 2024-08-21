3d6024383a17604710d7f1f4293ec4b148acbbd2
Jurema Vidal <juremavidal.jv62@gmail.com>
2024-08-21 21:52:27 +0200

    Set up basic pages, routing, and styling for stAIfit frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+function Header() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-      </ul>
-    </nav>
+    <header className="App-header">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
-};
+}
 
 export default Header;
